9c12d5f99393a47dbae89cccc103eb4ddf0cc1c7
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-10-03 02:57:18 +0800

    update

--- dist/assets/IndexView-2a2cccac.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-fa400f69.js";const o={};function r(t,s){return c(),n("div",null,"index")}const a=e(o,[["render",r]]);export{a as default};
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/online-store/',
+  // base: '/online-store/',
   plugins: [vue()],
   server: {
     port: 3000,
