1b5c7a1faed50c327b6c45062c0a459800aa14f9
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-10-03 21:49:58 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -45,10 +45,10 @@ const routes = [
     meta: { layout: 'blank' },
     component: () => import('@/modules/auth/views/SigninView.vue'),
   },
-  {
-    path: '/online-store/',
-    component: () => import('@/views/test.vue'),
-  },
+  // {
+  //   path: '/online-store/',
+  //   component: () => import('@/views/test.vue'),
+  // },
 ];
 
 const router = createRouter({
