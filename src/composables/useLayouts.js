5fdf83877aa809918788b926e303ac9b9259ef8d
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-10-03 22:09:14 +0800

    adding dist

--- src/composables/useLayouts.js
+++
@@ -1,9 +0,0 @@
-import MainLayout from '@/layouts/MainLayout.vue';
-import BlankLayout from '@/layouts/BlankLayout.vue';
-
-export function useLayouts() {
-  return {
-    main: MainLayout,
-    blank: BlankLayout,
-  };
-}
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-function capitalizeFirstLetter(str) {
-  const capitalizedStr = str.charAt(0).toUpperCase() + str.slice(1);
-
-  return capitalizedStr;
-}
-
-export default {
-  capitalizeFirstLetter,
-};
