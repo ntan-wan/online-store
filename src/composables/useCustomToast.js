ba3767cdaf04b9c1b3ee8ea9b9c35a7398f9ecbd
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-10-04 01:06:13 +0800

    UPDATE: updated toast lifespan

--- src/composables/useCustomToast.js
+++ src/composables/useCustomToast.js
@@ -8,16 +8,16 @@ export function useCustomToast() {
       severity: 'success',
       summary: 'Success',
       detail: msg,
-      life: 3000,
+      life: 1000,
     });
   }
 
   function showToastWarn(msg) {
-    toast.add({ severity: 'warn', summary: 'Warn', detail: msg, life: 2000 });
+    toast.add({ severity: 'warn', summary: 'Warn', detail: msg, life: 1000 });
   }
 
   function showToastError(msg) {
-    toast.add({ severity: 'error', summary: 'Error', detail: msg, life: 2000 });
+    toast.add({ severity: 'error', summary: 'Error', detail: msg, life: 1000 });
   }
 
   return { showToastSuccess, showToastError, showToastWarn };
