a1a805b48be6eba6b47f34f079872ae843be6309
wang190 <89770062+wangwang190@users.noreply.github.com>
2023-10-03 21:29:43 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/online-store/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/online-store/' : '/',
   plugins: [vue()],
   server: {
     port: 3000,
